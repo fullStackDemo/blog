453058a578e6934f70ea464f9a435c546e1d9224
master <master2011zhao@gmail.com>
2019-04-17 07:50:17 +0800

    bash

--- bash/task.sh
+++ bash/task.sh
@@ -4,33 +4,31 @@ cd ./cdn
 
 DATE=$(date +%Y%m%d%H%M)
 
-echo "$FILENAME"
-
 argument=$1
 
 bg1="sandbox"
 bg2="pre"
 
-case $argument in 
-  $bg1)
-    echo "select $argument"
-    FILENAME="app.sandbox.tar";
-    if [[ -f "$FILENAME" ]]; then
-      echo "update"
-      rm $FILENAME
-    fi
-    tar -cf $FILENAME ../public ../static
+case $argument in
+    $bg1)
+        echo "select $argument"
+        FILENAME="app.sandbox.tar";
+        if [[ -f "$FILENAME" ]]; then
+            echo "update"
+            rm $FILENAME
+        fi
+        tar -cf $FILENAME ../public ../static
     ;;
-  $bg2)
-    echo "select $argument"
-    FILENAME="app.$DATE.pre.tar";
-    if [[ -f "$FILENAME" ]]; then
-      echo "update"
-      rm $FILENAME
-    fi
-    tar -cf $FILENAME ../public
+    $bg2)
+        echo "select $argument"
+        FILENAME="app.$DATE.pre.tar";
+        if [[ -f "$FILENAME" ]]; then
+            echo "update"
+            rm $FILENAME
+        fi
+        tar -cf $FILENAME ../public
     ;;
-  *)
-    echo "you need to select a running backgound"
+    *)
+        echo "you need to select a running backgound"
     ;;
 esac
