dc823d73a0137cf4d1ec9f60d7cb96d86fce89e4
master <master2011zhao@gmail.com>
2019-04-21 22:37:45 +0800

    loop

--- less/gulpfile.js
+++ less/gulpfile.js
@@ -1,11 +1,13 @@
 const { watch, task, src, dest, series } = require('gulp')
 
 const gulpLess = require('gulp-less')
+const gulpLessChanged = require('gulp-less-changed')
 
 
 function less() {
   return src('./*.less')
     .pipe(dest('./less'))
+    .pipe(gulpLessChanged())
     .pipe(gulpLess({
       paths: ['./less/*.less']
     }))
