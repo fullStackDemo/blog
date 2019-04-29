26ef07cb7601b7c03363ad68b02136e7d98ac921
master <master2011zhao@gmail.com>
2019-04-29 08:15:26 +0800

    --

--- java/workspace/src/Main.java
+++ java/workspace/src/Main.java
@@ -1,6 +1,11 @@
+
+import com.data.type.DataType;
+
 public class Main {
 
     public static void main(String[] args) {
+        DataType dataType = new DataType();
         System.out.println("Hello World!");
+        System.out.println(dataType.getName());
     }
 }
--- java/workspace/src/com/data/type/DataType.java
+++ java/workspace/src/com/data/type/DataType.java
@@ -1,7 +1,18 @@
 package com.data.type;
 
 public class DataType {
+    // 全局变量
+    private static String s1 = "hello";
+
+    public String getName(){
+        return s1;
+    }
+
+    // public 权限修改符 static 静态修饰符 void 返回值修饰符
     public static void main(String[] args) {
-        System.out.println(999);
+        // 局部变量
+        String s2 = "Java";
+        System.out.println(s1);
+        System.out.println(s2);
     }
 }
