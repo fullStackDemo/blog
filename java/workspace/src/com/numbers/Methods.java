cbe848db4711683591a1406ce358aa1c4e6a3548
wangzhao <wangzhao@go-goal.com>
2019-05-09 18:06:07 +0800

    number methods

--- java/workspace/src/com/numbers/Methods.java
+++ java/workspace/src/com/numbers/Methods.java
@@ -57,7 +57,7 @@ public class Methods {
         System.out.println(Math.abs(Double.parseDouble("-88.88")));
 
         // ceil() return double type
-        double d1=-100.81;
+        double d1 = -100.81;
         float f1 = 99;
         System.out.println("Math.ceil()");
         System.out.println(Math.ceil(d1));
@@ -68,7 +68,86 @@ public class Methods {
         System.out.println(Math.floor(-99.99));
         System.out.println(Math.floor(88.10));
 
+        // rint()
+        System.out.println("Math.rint()");
+        System.out.println(Math.rint(99.1));
+        System.out.println(Math.rint(99.5));
+        System.out.println(Math.rint(99.6));
 
+        // round()
+        System.out.println("Math.round()");
+        System.out.println(Math.round(99.1));
+        System.out.println(Math.round(99.9));
+
+        // min()
+        System.out.println("Math.min()");
+        System.out.println(Math.min(11, 44.1));
+
+        // max()
+        System.out.println("Math.max()");
+        System.out.println(Math.max(11, 44.1));
+
+        // exp() 对数
+        System.out.println("Math.exp()");
+        System.out.println(Math.E);
+        System.out.println(Math.exp(2));
+
+        // log()
+        System.out.println("Math.log()");
+        System.out.println(Math.log(7.39));
+
+        // pow
+        System.out.println("Math.pow");
+        System.out.println(Math.pow(2, 5));
+
+        // sqrt
+        System.out.println("Math.sqrt");
+        System.out.println(Math.sqrt(2));
+
+        // sin
+        double degrees = 60.0;
+        double radians = Math.toRadians(degrees);
+
+        System.out.println("Math.sin()");
+        System.out.println(radians);
+        System.out.println(Math.sin(radians));
+        System.out.println(Math.sqrt(3) / 2);
+
+        // cos
+        System.out.println("Math.cos()");
+        System.out.println(Math.cos(radians));
+
+        // tan
+        System.out.println("Math.tan()");
+        System.out.println(Math.tan(radians));
+
+        // asin
+        System.out.println("Math.asin()");
+        System.out.println(Math.asin(Math.sin(radians)) == radians);
+
+        // acos
+        System.out.println("Math.acos()");
+        System.out.println(Math.acos(Math.cos(radians)) == radians);
+
+        // atan
+        System.out.println("Math.atan()");
+        System.out.println(Math.atan(Math.tan(radians)) == radians);
+
+        // atan2
+        System.out.println("Math.atan2()");
+        System.out.println(Math.atan2(30, 45));
+
+        //toDegrees
+        System.out.println("Math.toDegrees()");
+        System.out.println(Math.round(Math.toDegrees(radians)));
+
+        //toRadians
+        System.out.println("Math.toRadians()");
+        System.out.println(Math.toRadians(degrees));
+
+        //random
+        System.out.println("Math.random()");
+        System.out.println(Math.round(Math.random() * 1000 + 1));
 
     }
 }
