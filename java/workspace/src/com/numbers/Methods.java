12486c691aa7ddb5130b9138ae9f821bcc2f1e7a
wangzhao <wangzhao@go-goal.com>
2019-05-09 16:30:19 +0800

    11

--- java/workspace/src/com/numbers/Methods.java
+++ java/workspace/src/com/numbers/Methods.java
@@ -33,7 +33,42 @@ public class Methods {
         System.out.println(x.equals(m));
 
 
-        //
+        // valueOf()
+        System.out.println("Number.valueOf()");
+        System.out.println(Double.valueOf("99"));
+        System.out.println(Float.valueOf("90"));
+        System.out.println(Long.valueOf("999"));
+        System.out.println(Integer.valueOf("9", 16));
+
+        // toString
+        System.out.println("Number.toString()");
+        String s1 = Integer.toString(8);
+        System.out.println(s1 instanceof String);
+
+        // parseInt()
+        System.out.println("parseInt()");
+        System.out.println(Integer.parseInt("666"));
+        System.out.println(Double.parseDouble("888"));
+        System.out.println(Float.parseFloat("777"));
+
+        // abs()
+        System.out.println("Math.abs()");
+        System.out.println(Math.abs(Integer.parseInt("-99")));
+        System.out.println(Math.abs(Double.parseDouble("-88.88")));
+
+        // ceil() return double type
+        double d1=-100.81;
+        float f1 = 99;
+        System.out.println("Math.ceil()");
+        System.out.println(Math.ceil(d1));
+        System.out.println(Math.ceil(f1));
+
+        // floor()
+        System.out.println("Math.floor()");
+        System.out.println(Math.floor(-99.99));
+        System.out.println(Math.floor(88.10));
+
+
 
     }
 }
