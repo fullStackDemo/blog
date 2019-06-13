ce7baee0009fe78909e4a9f8d83d243705652568
master <master2011zhao@gmail.com>
2019-06-14 07:41:59 +0800

    --

--- python/demo.py
+++ python/demo.py
@@ -1,3 +1,9 @@
-import urllib2
-response = urllib2.urlopen('http://www.example.com/')
-html = response.read();
+#!/usr/bin/python
+
+print "Hello, World!"
+
+name = "999"
+if name:
+  print name;
+
+a,b=0,1;
