041a08d9cdc71a8a79db9288970c3fa397a1e0fc
master <master2011zhao@gmail.com>
2019-07-15 00:17:39 +0800

    --

--- 后台接口开发/Java/initial/src/main/java/hello/Application.java
+++ 后台接口开发/Java/initial/src/main/java/hello/Application.java
@@ -1,10 +1,19 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
+import javax.swing.*;
+
+@RestController
 @SpringBootApplication
 public class Application {
+    @RequestMapping("/")
+    String home(){
+        return "Hello World!";
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
