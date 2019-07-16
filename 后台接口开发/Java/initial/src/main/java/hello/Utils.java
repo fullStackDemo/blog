a4bf681efa1af7848b516981b7d1c4095c1ef03c
master <master2011zhao@gmail.com>
2019-07-17 00:20:10 +0800

    comment

--- 后台接口开发/Java/initial/src/main/java/hello/Utils.java
+++ 后台接口开发/Java/initial/src/main/java/hello/Utils.java
@@ -9,14 +9,31 @@ import java.util.Map;
 
 public class Utils {
     
-    public static String toJSON(int httpCode, int dataCode, List list){
+    //  格式化 map
+    public static String toJSON(Map map) {
+        return new Gson().toJson(map);
+    }
+    
+    //  格式化 list
+    public static String toJSON(List list) {
+        return new Gson().toJson(list);
+    }
+    
+    // 格式化返回结果
+    public static String toJSON(int httpCode, int dataCode, List list) {
         Map result = new HashMap<>();
+        // http status code
         result.put("code", httpCode);
+        //  data
         Map data = new HashMap();
+        //  response status code
         data.put("code", dataCode);
+        //  response data
         data.put("data", list);
+        
         result.put("data", data);
-        return new Gson().toJson(result);
+        
+        return toJSON(result);
     }
     
 }
