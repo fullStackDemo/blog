7350e50bcea5fc7e341e57c1d0dbad8621e8b5a7
master <master2011zhao@gmail.com>
2019-07-21 00:20:32 +0800

    service

--- 后台接口开发/Java/initial/src/main/java/hello/MysqlTestController.java
+++ 后台接口开发/Java/initial/src/main/java/hello/MysqlTestController.java
@@ -2,10 +2,12 @@ package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path = "/mysql")
@@ -28,8 +30,67 @@ public class MysqlTestController {
     
     @GetMapping("/all")
     @ResponseBody
-    public Iterable<User> getAllUsers(){
+    public Map getAllUsers() {
         // This returns a JSON or XML with the users
-        return  userRepository.findAll();
+        Iterable results = userRepository.findAll();
+        System.out.println(results);
+        ArrayList<User> arrayList = new ArrayList<User>();
+        for (Object user : results) {
+            User currentUser = (User) user;
+            arrayList.add(currentUser);
+        }
+        Map result = new HashMap();
+        result.put("code", 0);
+        result.put("data", arrayList);
+        return result;
+    }
+    
+    //  find user
+    @GetMapping("/find")
+    @ResponseBody
+    public Object getUser(@RequestParam Integer id) {
+        System.out.println(id);
+        Map result = new HashMap();
+        result.put("code", 0);
+        if (id == null) {
+            result.put("message", "缺少id");
+            return result;
+        }
+        User user = userRepository.findById(id).get();
+        result.put("data", user);
+        return result;
+    }
+    
+    //  update user
+    @GetMapping("/update")
+    @ResponseBody
+    public Object updateUser(@RequestParam int id, @RequestParam String name, @RequestParam String email) {
+        System.out.println(id);
+        Map result = new HashMap();
+        result.put("code", 0);
+//        if(id == 0){
+//            result.put("message", "缺少id");
+//            return result;
+//        }else if(name == null && email == null){
+//            result.put("message", "name, email不能同时为空");
+//            return result;
+//        }
+        Optional _user = userRepository.findById(id);
+        if (_user.isPresent()) {
+            User user =(User) _user.get();
+            user.setName(name);
+            user.setEmail(email);
+            result.put("data", user);
+            result.put("message", "update successfull");
+            userRepository.save(user);
+        }
+        return result;
+    }
+    
+    // error
+    @GetMapping("/error")
+    @ResponseBody
+    public Exception error(Exception err) {
+        return err;
     }
 }
---
+++ 后台接口开发/Java/initial/src/main/java/hello/UserService.java
@@ -0,0 +1,63 @@
+package hello;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    
+    @Resource
+    private UserRepository userRepository;
+    
+    /*
+     * save, update,delete 方法需要绑定事务. 使用@Transactional进行事务的绑定.
+     * @param User
+     * 保存对象
+     */
+    @Transactional
+    public void save(User user) {
+        userRepository.save(user);
+    }
+    
+    /*
+     *  根据 id 删除对象
+     *  @params id
+     */
+    @Transactional
+    public void delete(int id) {
+        userRepository.deleteById(id);
+    }
+    
+    /*
+     * find all
+     * @return
+     * */
+    @Transactional
+    public Iterable<User> getAll() {
+        return userRepository.findAll();
+    }
+    
+    /*
+    * update
+    *
+    * */
+    @Transactional
+    public void update(User user){
+        userRepository.save(user);
+    }
+    
+    /*
+    * findOneById
+    * @return
+    * */
+    @Transactional
+    public Object findOneByName(int id){
+        return userRepository.findById(id);
+    }
+    
+    
+    
+}
