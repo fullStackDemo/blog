116ee2521feae48644498be9d99716075d33077b
master <master2011zhao@gmail.com>
2019-07-31 21:17:31 +0800

    char

--- 后台接口开发/Java/initial/src/main/java/hello/MD5Utils.java
+++ 后台接口开发/Java/initial/src/main/java/hello/MD5Utils.java
@@ -15,7 +15,7 @@ public class MD5Utils {
 	 * @param readyEncryptStr ready encrypt string
 	 * @return String encrypt result string
 	 */
-	public  String MD5_16bit(String readyEncryptStr) throws NoSuchAlgorithmException {
+	public String MD5_16bit(String readyEncryptStr) throws NoSuchAlgorithmException {
 		if (readyEncryptStr != null) {
 			System.out.println(MD5_32bit(readyEncryptStr));
 			return MD5_32bit(readyEncryptStr).substring(8, 24);
@@ -31,7 +31,7 @@ public class MD5Utils {
 	 * @return String encrypt result string
 	 * @throws NoSuchAlgorithmException
 	 */
-	public  static   String MD5_32bit(String readyEncryptStr) throws NoSuchAlgorithmException {
+	public static String MD5_32bit(String readyEncryptStr) throws NoSuchAlgorithmException {
 		if (readyEncryptStr != null) {
 			//Get MD5 digest algorithm's MessageDigest's instance.
 			MessageDigest md = MessageDigest.getInstance(ALGORITHM_MD5);
@@ -55,16 +55,54 @@ public class MD5Utils {
 		}
 	}
 	
+	public static final String MD5_32bit1(String readyEncryptStr) throws NoSuchAlgorithmException {
+		if (readyEncryptStr != null) {
+			//The cipher text converted to hexadecimal string
+			StringBuilder su = new StringBuilder();
+			//Get MD5 digest algorithm's MessageDigest's instance.
+			MessageDigest md = MessageDigest.getInstance(ALGORITHM_MD5);
+			byte[] b = md.digest(readyEncryptStr.getBytes());
+			int temp = 0;
+			//byte array switch hexadecimal number.
+			for (int offset = 0, bLen = b.length; offset < bLen; offset++) {
+				temp = b[offset];
+				if (temp < 0) {
+					temp += 256;
+				}
+				int d1 = temp / 16;
+				int d2 = temp % 16;
+				su.append(Integer.toHexString(d1) + Integer.toHexString(d2));
+			}
+			return su.toString();
+		} else {
+			return null;
+		}
+	}
+	
+	/**
+	 * transform  char to ascii
+	 */
+	public static String transformToAscII(String readyEncryptStr) {
+		byte[] bytes = readyEncryptStr.getBytes();
+		String result = "";
+		for (int i = 0; i < bytes.length; i++) {
+			System.out.println(bytes[i]);
+			result += bytes[i];
+		}
+		return result;
+	}
+	
+	
 	// test
-//	public static void main(String[] args) {
-//		try {
-//			String md516 = MD5Utils.MD5_16bit("admin123!");
-//			System.out.println("16bit-md5:\n" + md516);
-//			String md532 = MD5Utils.MD5_32bit("admin123!");
-//			System.out.println("32bit-md5:\n" + md532);
-//
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
+	public static void main(String[] args) {
+		try {
+			String testStr = "test1qaz2wsx@123test123";
+			String result = transformToAscII(testStr);
+			String md532 = MD5Utils.MD5_32bit(result);
+			System.out.println("32bit-md5:\n" + md532);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
