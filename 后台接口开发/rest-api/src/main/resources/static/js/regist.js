0f88f0c4205b9408280db9ea6cf9593087d158eb
master <master2011zhao@gmail.com>
2019-12-01 10:17:45 +0800

    md5

--- 后台接口开发/rest-api/src/main/resources/static/js/regist.js
+++ 后台接口开发/rest-api/src/main/resources/static/js/regist.js
@@ -1,31 +1,44 @@
-//获取相关用户信息
+// 获取相关用户信息
 const userNameInput = document.getElementById("js_input——user");
 const passwordInput = document.getElementById("js_input——pwd");
 const passwordConfirmInput = document.getElementById("js_input——pwd2");
 const submitBtn = document.getElementById("submit");
 
-//submit
+// submit
 submitBtn.onclick = () => {
+
 	const userName = userNameInput.value;
 	const password = passwordInput.value;
 	const confirmPassword = passwordConfirmInput.value;
 
-	//verify
+	// verify
 	if (!userName) {
 		weui.topTips('用户姓名不能为空');
 		return;
 	} else if (!password) {
 		weui.topTips('用户密码不能为空');
 		return;
-	} else if (confirmPassword != password) {
+	} else if (confirmPassword !== password) {
 		weui.topTips('前后密码不一致，请重试');
 		return;
 	}
 
-	//注册
+	// 加密密码
+	const salt="1qaz2wsx3edc4rfv5tgb6yhn7ujm8ik9ol0p@!.";
+	const asciStr = userName + salt + password;
+	const asciArr = asciStr.split('');
+	const asciResult = [];
+	asciArr.forEach(n =>{
+		asciResult.push(n.charCodeAt());
+	});
+	const ascireusltStr = asciResult.join(salt);
+	const newPassword = hex_md5(ascireusltStr);
+
+
+	// 注册
 	dataService.addUser({
 		userName,
-		password,
+		password: newPassword,
 	}).then(res => {
 
 	})
