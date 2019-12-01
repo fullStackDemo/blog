e2b93aa33fa6575fc1a502630225faa5ba1f93bd
master <master2011zhao@gmail.com>
2019-12-01 09:41:17 +0800

    000

--- 后台接口开发/rest-api/src/main/java/com/zz/entity/User.java
+++ 后台接口开发/rest-api/src/main/java/com/zz/entity/User.java
@@ -8,6 +8,16 @@ public class User {
     
     private String token;
     
+    private String password;
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
     public String getToken() {
         return token;
     }
--- 后台接口开发/rest-api/src/main/java/com/zz/utils/JWTUtils.java
+++ 后台接口开发/rest-api/src/main/java/com/zz/utils/JWTUtils.java
@@ -1,43 +1,81 @@
 package com.zz.utils;
 
+import com.zz.entity.User;
 import io.jsonwebtoken.*;
 import org.apache.commons.codec.binary.Base64;
 
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class JWTUtils {
     
+    // 生成签名的时候使用的秘钥secret
     private static final String SECRETKEY = "KJHUhjjJYgYUllVbXhKDHXhkSyHjlNiVkYzWTBac1Yxkjhuad";
+    
+    // expirationDate 生成jwt的有效期，单位秒
+    private static long expirationDate = 2 * 60 * 60;
+    
     
     /**
      * 由字符串生成加密key
      *
-     * @return
+     * @return SecretKey
      */
-    public static SecretKey generalKey(String stringKey) {
+    private static SecretKey generalKey(String stringKey) {
         byte[] encodedKey = Base64.decodeBase64(stringKey);
-        SecretKey key = new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
-        return key;
+        return new SecretKeySpec(encodedKey, 0, encodedKey.length, "AES");
     }
     
     /**
-     * 创建jwt
+     * 创建 jwt
      *
-     * @param uuid             唯一id，uuid即可
-     * @param subject        json形式字符串或字符串，增加用户非敏感信息存储，如用户id或用户账号，与token解析后进行对比，防止乱用
-     * @param expirationDate 生成jwt的有效期，单位秒
+     * @param user 登录成功后的用户信息
      * @return jwt token
-     * @throws Exception
      */
-    public static String createJWT(String uuid, String subject, long expirationDate) throws Exception {
+    public static String createToken(User user) {
+        
+        // 指定签名的时候使用的签名算法，也就是header那部分，jwt已经将这部分内容封装好了
         SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        
+        // 生成JWT的时间
         long nowMillis = System.currentTimeMillis();
         Date now = new Date(nowMillis);
-        SecretKey key = generalKey(SECRETKEY);
-        JwtBuilder builder = Jwts.builder().setIssuer("").setId(uuid).setIssuedAt(now).setSubject(subject)
+        
+        // 创建payload的私有声明（根据特定的业务需要添加，如果要拿这个做验证，一般是需要和jwt的接收方提前沟通好验证方式的）
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", user.getUserId());
+        claims.put("userName", user.getUserName());
+        claims.put("password", user.getPassword());
+        
+        // 生成签名的时候使用的秘钥secret,这个方法本地封装了的，一般可以从本地配置文件中读取，切记这个秘钥不能外露哦。它就是你服务端的私钥，在任何场景都不应该流露出去。一旦客户端得知这个secret, 那就意味着客户端是可以自我签发jwt了
+        SecretKey key = generalKey(SECRETKEY + user.getPassword());
+        
+        // 生成签发人
+        // json形式字符串或字符串，增加用户非敏感信息存储，如用户id或用户账号，与token解析后进行对比，防止乱用
+        HashMap<String, Object> storeInfo = new HashMap<String, Object>();
+        storeInfo.put("userId", user.getUserId());
+        storeInfo.put("userName", user.getUserName());
+        String subject = storeInfo.toString();
+        
+        // 下面就是在为payload添加各种标准声明和私有声明了
+        // 这里其实就是new一个JwtBuilder，设置jwt的body
+        JwtBuilder builder = Jwts.builder()
+                // 如果有私有声明，一定要先设置这个自己创建的私有的声明，这个是给builder的claim赋值，一旦写在标准的声明赋值之后，就是覆盖了那些标准的声明的
+                .setClaims(claims)
+                // 唯一随机UUID
+                // 设置JWT ID：是JWT的唯一标识，根据业务需要，这个可以设置为一个不重复的值，主要用来作为一次性token,从而回避重放攻击
+                .setId(UUID.randomUUID().toString())
+                // jwt的签发时间
+                .setIssuedAt(now)
+                // 代表这个JWT的主体，即它的所有人，这个是一个json格式的字符串，可以存放什么userid，roldid之类的，作为什么用户的唯一标志
+                .setSubject(subject)
+                // 设置签名使用的签名算法和签名使用的秘钥
                 .signWith(signatureAlgorithm, key);
+        
         if (expirationDate >= 0) {
             long expMillis = nowMillis + expirationDate * 1000;
             Date exp = new Date(expMillis);
@@ -47,15 +85,39 @@ public class JWTUtils {
     }
     
     /**
-     * 解密jwt，获取实体
+     * 解密token，获取声明的实体
      *
-     * @param jwt
+     * @param token 加密后的token
+     * @return claims
      */
-    public static Claims parseJWT(String jwt) throws ExpiredJwtException, UnsupportedJwtException,
-            MalformedJwtException, SignatureException, IllegalArgumentException {
-        SecretKey key = generalKey(SECRETKEY);
-        Claims claims = Jwts.parser().setSigningKey(key).parseClaimsJws(jwt).getBody();
+    private static Claims parseToken(String token, User user) {
+        // 签名秘钥，和生成的签名的秘钥要保持一模一样
+        SecretKey key = generalKey(SECRETKEY + user.getPassword());
+        
+        // 获取私有声明
+        Claims claims = Jwts.parser()
+                // 设置签名的秘钥
+                .setSigningKey(key)
+                // 设置需要解析的token
+                .parseClaimsJws(token).getBody();
+        
         return claims;
+    }
+    
+    
+    /**
+     * 校验token
+     *
+     * @param token 加密后的token
+     * @param user  用户信息
+     * @return true|false
+     */
+    public static Boolean verify(String token, User user) {
+        
+        // 获取私有声明的实体
+        Claims claims = parseToken(token, user);
+        
+        return claims.get("password").equals(user.getPassword());
     }
     
     
