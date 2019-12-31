00ecbe53588a2adbf22f438be66934d23dcd1f75
master <master2011zhao@gmail.com>
2019-12-31 16:25:45 +0800

    ---

--- 后台接口开发/Java/demo/src/main/java/com/zz/mapper/StudentMapper.java
+++
@@ -1,8 +0,0 @@
-package com.zz.mapper;
-
-import com.zz.query.StudentQuery;
-import org.apache.ibatis.annotations.Mapper;
-
-public interface StudentMapper {
-    public int insert(StudentQuery query);
-}
